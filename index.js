bfafcd50d5dc97341f8d4caec67c73a521102efe
Ernest Kamau <1.kamauernest@gmail.com>
2025-03-08 08:09:07 +0300

    Completes codegrade

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let messages = []; // Create an empty array to store messages
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+// Function to count down from a given number to zero
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--; // Decrement the number
+  }
+}
